009d722d6425f4da4098a078b407099da842c47b
Seven <zhangqishan@yiqijiao.cn>
2019-05-06 17:08:44 +0800

    [ add ] add pagination

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { gql } from "apollo-boost";
 import styles from './Home.module.scss';
 import Article from 'components/Article';
-import { Input } from 'antd';
+import { Input, Pagination } from 'antd';
 import { Query }from '@/api/index.js'
 import * as R from 'ramda';
 
 const Search = Input.Search;
+const PageSize = 10;
 
 const GET_ARTICLES_AND_TAGS = gql`
-  query fetchData( $tags: Boolean!, $title: String, $tag: String) {
-    articleList( title: $title, tag: $tag ) {
+  query fetchData( $tags: Boolean!, $title: String, $tag: String, $start: Int) {
+    articleList( title: $title, tag: $tag, start: $start ) {
       totalCount
       articles {
         _id
@@ -38,13 +39,16 @@ function Tag ({ data, onTagClick }) {
 
 function Home() {
   const [ articles, setArticles ] = useState([]);
+  const [ total, setTotal ] = useState(0);
   const [ tags, setTags ] = useState([]);
   const [ query, setQuery ] = useState({ tags: true });
+  const queryChange = v => setQuery(R.merge(query, v));
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await Query(GET_ARTICLES_AND_TAGS, query);
       setArticles(data.articleList.articles);
+      setTotal(data.articleList.totalCount);
       if (data.tags) {
         setTags(data.tags);
       }
@@ -57,18 +61,19 @@ function Home() {
     <div className={ styles.home }>
       <div className={ styles.homeList }>
         { articles.map(item => (<Article key={item._id.toString()} data={item} />)) }
+        <Pagination defaultCurrent={1} total={total} onChange={ page  =>  queryChange({ start: (page - 1) * PageSize })}/>
       </div>
 
       <div className={ styles.homeSearch }>
         <div className={ styles.query }>
           <Search
             placeholder="请输入文章名称"
-            onSearch={ value => setQuery(R.merge(query, { tags: false, title: value })) }
+            onSearch={ value => queryChange({ tags: false, title: value }) }
           />
         </div>
         <div>
           {
-            tags.map(o => (<Tag key={o.name} data={o} onTagClick={ value => setQuery(R.merge(query, value))}></Tag>))
+            tags.map(o => (<Tag key={o.name} data={o} onTagClick={ value => queryChange(value)}></Tag>))
           }
         </div>
       </div>
